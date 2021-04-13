c6f427456837cad856680855c1b47f0a3dcc5d59
Oleh Shpaner <oshpaner@oshpaner-mbp.local>
2021-04-13 15:07:47 +0300

    half an hour test task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'login-form';
+export class AppComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    this.authService.login(
+      this.loginForm.controls.email.value,
+      this.loginForm.controls.password.value
+    ).subscribe(data => {
+      console.log(data);
+    })
+  }
 }
